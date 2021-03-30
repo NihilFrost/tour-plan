6c4ea3e5482307373cfd4fc8d6493c7f9110e585
NihilFrost <werfoster@yandex.ru>
2021-03-31 02:51:39 +0300

    fix: validation check

--- js/main.js
+++ js/main.js
@@ -46,24 +46,28 @@ $(document).ready(function() {
     modalButton.on('click', openModal);
     closeModalButton.on('click', closeModal);
 
-    $(".modal__form").validate({
-        errorClass: "invalid",
-        messages: {
-            name: {
-                required: "Укажите имя",
-                minlength: "Имя не должно быть короче 2 символов",
+    // each - для каждой формы с классом form запустить функцию проверки ИМЕННО ЭТОЙ формы
+    $(".form").each(function() {
+        $(this).validate({
+            errorClass: "invalid",
+            messages: {
+                name: {
+                    required: "Please enter your name",
+                    minlength: "The name must be at least 2 characters long",
+                },
+                email: {
+                    required: "Please enter your e-mail",
+                    email: "Your email address must be in the format of name@domail.com",
+                },
+                phone: {
+                    required: "Please enter your phone",
+                },
+                message: {
+                    required: "The message field is required ",
+                    minlength: "The message must be at least 10 characters long"
+                }
             },
-            email: {
-                required: "We need your email address to contact you",
-                email: "Your email address must be in the format of name@domail.com",
-            },
-            phone: {
-                required: "Телефон обязателен",
-            },
-        },
-
-
-    }); // validate
-
+        }); // validate
+    }); // function
 
 }); // jQuery
